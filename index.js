4d05adb94bf39939a4a1cda9d0e4e67c26556338
bhaden94 <bhaden94@yahoo.com>
2021-02-03 18:56:11 -0800

    post and get routes for translation

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import { geeksay, getRandomQuote, getRandomTranslation } from "./helpers.js";
 import express from "express";
 const app = express();
+app.use(express.text());
 const port = 4000;
 
-app.get("/", (req, res) => {
-	res.send(getRandomTranslation());
+// translate quote to geek
+app.post("/", (req, res) => {
+	res.send(geeksay(req.body));
 });
 
+// get a randow quote and see it translated to geek
 app.get("/random", (req, res) => {
 	const randomQuote = getRandomQuote();
 	res.send(`${randomQuote} -> ${geeksay(randomQuote)}`);
